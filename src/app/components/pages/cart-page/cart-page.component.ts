7038cbbba2bb211276af1360fdf1629ab02c84ae
Nikiforos Ioannidis <nioannidis@outlook.com>
2024-05-19 13:43:58 +0300

    fix cart component

--- src/app/components/pages/cart-page/cart-page.component.ts
+++ src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Cart } from '../../../shared/models/Cart';
+import { CartService } from '../../../services/cart.service';
+import { CartItem } from '../../../shared/models/CartItem';
+import { TitleComponent } from '../../partials/title/title.component';
+import { CommonModule, NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, CommonModule, NgFor, RouterLink],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
 export class CartPageComponent {
+  cart!: Cart;
+  constructor(private cartService: CartService) {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    })
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id)
+  }
+
+  changeQuantity(cartItem:CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity)
+  }
 
 }
--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  cartQuantity = 0;
+  constructor(cartService: CartService) {
+    cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+    })
+  }
 
 }
